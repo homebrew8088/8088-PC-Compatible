764763560f41985a9d90595f1aa5958a4f9b9fce
homebrew8088 <64012455+homebrew8088@users.noreply.github.com>
2021-11-04 19:01:34 -0600

    Update dma.asm

--- asm/small code/DMA test/dma.asm
+++ asm/small code/DMA test/dma.asm
@@ -15,16 +15,16 @@ START:
 	MOV AL, 0XCB			; CASCADE mode, verify, channel 3 ?? CHECKS HOLD FUNCTION
 	OUT 0X0B, AL 			; DMA Mode register
  	MOV AL, 0X00			; DMA Command register bits:
- 				; DACK active low, DREQ active high,
- 				; late write, fixed priority,
- 				; normal timing, controller enable
- 				; channel 0 addr hold disable
- 				; memory to memory disable
+ 					; DACK active low, DREQ active high,
+ 					; late write, fixed priority,
+ 					; normal timing, controller enable
+ 					; channel 0 addr hold disable
+ 					; memory to memory disable
  	OUT 0X08, AL			; DMA Command register
 	OUT 0X81, AL			; DMA Page, channel 2
  	OUT 0X82, AL			; DMA Page, channel 3
-				;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-				;SET UP CHANNEL 1			
+					;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+					;SET UP CHANNEL 1			
 	MOV AL, 0XFF			;
 	OUT 0X0C, AL			;RESET FLIP-FLOP
 	MOV AL, 0X60			;LOW BYTE ADDRESS 
@@ -41,7 +41,7 @@ START:
 
 	MOV AL, 0X07			;PAGE 0X0# ADDRESS 0X#0000
  	OUT 0X83, AL			; DMA Page, channels 0,1
-				;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+					;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	MOV AL, 0X01
 	OUT 0X0A, AL			; unmask DMA channel 1
 	MOV AL, 0X03
